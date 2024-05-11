f8cd8406cce14a3d1838c67dcfd0b89103399908
flavio1974 <flavioeduardos@hotmail.com>
2024-05-11 13:05:32 -0300

    ajuste login decode e encode (utf8)

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -19,7 +19,7 @@ def homepage():
     form_login = FormLogin()
     if form_login.validate_on_submit():
         usuario = Usuario.query.filter_by(email=form_login.email.data).first()
-        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+        if usuario and bcrypt.check_password_hash(usuario.senha.encode("utf8"), form_login.senha.data):
             login_user(usuario)
             return redirect(url_for("perfil", id_usuario=usuario.id))
 
@@ -30,7 +30,7 @@ def homepage():
 def criar_conta():
     form_criarconta = FormCriarConta()
     if form_criarconta.validate_on_submit():
-        senha = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        senha = bcrypt.generate_password_hash(form_criarconta.senha.data).decode("utf8")
         usuario = Usuario(username=form_criarconta.username.data,
                           senha=senha, email=form_criarconta.email.data)
 
